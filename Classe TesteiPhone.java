df99baab42a56352a5a691ef70c6600c1192db58
usernamerandomman <sa3bm2020@gmail.com>
2024-06-26 10:22:59 -0300

    Classe TesteiPhone.java

---
+++ Classe TesteiPhone.java
@@ -0,0 +1,9 @@
+public class TesteiPhone {
+    public static void main(String[] args) {
+        iPhone meuiPhone = new iPhone();
+
+        meuiPhone.tocarMusica();
+        meuiPhone.fazerChamada("123456789");
+        meuiPhone.navegar("https://www.example.com");
+    }
+}
